dacf42740d62342ab021c1d1e8ef2d0bb322585c
Mikhail <luckyboy13@mail.ru>
2018-12-02 05:10:20 +0300

    Detection pipeline added.

--- hc/detectionPipeline.py
+++ hc/detectionPipeline.py
@@ -2,12 +2,14 @@
 import os
 import sys
 
+from hc.detectors.BaseDetector import BaseDetector
+
 sys.path.append('./detectors/')
 # import tensorflow as tf
 import cv2
 import time
 import datetime
-from detectors.human_detector import HumanDetector
+from hc.detectors.human_detector import HumanDetector
 import json
 import logging
 import argparse
@@ -15,6 +17,7 @@ from pprint import pprint
 
 LOG_LEVEL = logging.INFO
 FPSD = 5
+CNTR_DELAY = 50
 
 
 def callback(rule2score):
@@ -29,7 +32,7 @@ class detectionPipeline(BaseDetector):
         logging.basicConfig(level=LOG_LEVEL)
 
         # init algs with model paths
-        for i, k in enumerate(self.available_rules2alg.keys())
+        for i, k in enumerate(self.available_rules2alg.keys()):
             m_path = models_paths[i]
             self.available_rules2alg[k] = self.available_rules2alg[k](m_path)
 
@@ -53,7 +56,7 @@ class detectionPipeline(BaseDetector):
                     logging.warning(f'{rule} rule is not in `available rules`')
                 else:
                     if rule not in self.rule2alg.keys() or num[0] < self.rule2alg[rule][1]:
-                        self.rule2alg[rule] = [self.available_rules2alg[rule], num[0], num[1]]
+                        self.rule2alg[rule] = (self.available_rules2alg[rule], num[0], num[1])
                         # self.rule2score[rule] = None
 
         logging.info(self.rule2alg)
@@ -63,8 +66,9 @@ class detectionPipeline(BaseDetector):
         rule2score = {}
         imframe = img.copy()
         # iterate events
-        for rule, alg, bbox_num, threshold in self.rule2alg.items():
+        for rule, (alg, bbox_num, threshold) in self.rule2alg.items():
             rule2score[rule] = 0
+            rule2score['alert'] = 0
 
             # find bboxes for event
             boxes, scores, classes, num = alg.processFrame(img)
@@ -76,13 +80,13 @@ class detectionPipeline(BaseDetector):
                 for i in range(len(boxes)):
                     if classes[i] == 1 and scores[i] > threshold:
                         rule2score[rule] = scores[i] if rule2score[rule] < scores[i] else rule2score[rule]
-
+                        rule2score['alert'] = 1
                         box = boxes[i]
                         cv2.rectangle(imframe, (box[1], box[0]), (box[3], box[2]), (255, 0, 0), 2)
                     else:
                         rule2score[rule] = 0
-
-        logging.info(f'Rule2score after RULE FILL = \n{rule2score}\n')
+        # if rule2score['alert'] == 1:
+        #     logging.info(f'Rule2score after RULE FILL = \n{rule2score}\n')
         glob_alert_list = []
 
         for rule_grp in self.config['rules']:
@@ -99,8 +103,8 @@ class detectionPipeline(BaseDetector):
             if al:
                 rule2score['alert'] = al
                 rule2score['alert_desc'] = self.config['rules'][i]
-
-        logging.info(f'Rule2score after ALERT FILL = \n{rule2score}\n')
+        # if rule2score['alert'] == 1:
+        #     logging.info(f'Rule2score after ALERT FILL = \n{rule2score}\n')
 
         rule2score['cam_id'] = self.config['cam_id']
         rule2score['time'] = datetime.datetime.now()
@@ -109,23 +113,30 @@ class detectionPipeline(BaseDetector):
         # pass
 
     def processVideo(self):
-        int
         fps = 0
+        cntr = 0
         while True:
             if fps % FPSD == 0:
                 r, img = self.cap.read()
+                if img is None:
+                    break
                 img = cv2.resize(img, (1280, 720))
                 rule2score = self.processFrame(img=img)
-                logging.info(f'FINAL Rule2score after process video = \n{rule2score}\n')
+                # if rule2score['alert'] == 1:
+                #     logging.info(f'FINAL Rule2score after process video = \n{rule2score}\n')
                 if self.outlier:
                     if rule2score['alert']:
                         continue
                     else:
                         self.outlier = False
                         callback(rule2score=rule2score)
-                elif rule2score['alert']:
+                        cntr = 0
+                elif rule2score['alert'] and cntr >= CNTR_DELAY:
+                    logging.info(f'FINAL Rule2score after process video = \n{rule2score}\n')
                     self.outlier = True
                     callback(rule2score=rule2score)
+                else:
+                    cntr += 1
             fps += 1
             key = cv2.waitKey(1)
             if key & 0xFF == ord('q'):
@@ -149,5 +160,7 @@ if __name__ == "__main__":
     parser = process_args()
     args = parser.parse_args()
 
-    model_path = os.path.join('.', 'ssd_mobilenet_v1_coco_2018_01_28/frozen_inference_graph.pb')
-    pipe = detectionPipeline('./', './config_ex.json', models_paths=model_path)
+    model_path = os.path.join('/Users/lucky13/Downloads', 'ssd_mobilenet_v1_coco_2018_01_28/frozen_inference_graph.pb')
+    pipe = detectionPipeline('/Users/lucky13/Downloads/Hack/l_05_persons_0_smoke_1_01.mp4', './config_ex.json', models_paths=[model_path])
+
+    pipe.processVideo()
